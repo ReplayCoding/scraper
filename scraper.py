e529f7d596e6c54f5ef67192e41882d1d633f977
ReplayCoding <saeedcamposijaz@gmail.com>
2017-08-17 12:48:42 -0400

    Initial Commit

---
+++ scraper.py
@@ -0,0 +1,29 @@
+import html.parser
+import re
+import urllib.request
+import threading
+import sys
+
+"A simple web scraper"
+class Scraper:
+    def run(self, site):
+        print("Running")
+        self.links = set()
+        sites = self.scrape(site)
+        return self.links
+    def scrape(self, data):
+        try:
+            with urllib.request.urlopen(data) as response:
+                data = str(response.read())
+            link_re = re.compile("[A-Za-z]+://[A-Za-z0-9-_]+.[A-Za-z0-9-_:%&;\?#/.=]+")
+            links = link_re.findall(data)
+            for link in links:
+                self.links.add(link)
+                x = threading.Thread(target=self.scrape, args=(link,))
+                x.start()
+                print(link)
+        except:
+            pass
+if __name__ == "__main__":
+    scraper = Scraper()
+    data = scraper.run(input())
